ff2916a25268b5a375129e08ca2dc371f337416f
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2026-06-28 16:47:54 -0300

    feat: integrate BoardTable into Board page

--- taskflow/src/app/pages/board/board.ts
+++ taskflow/src/app/pages/board/board.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Button } from '../../components/ui/button/button';
 import { Avatar } from '../../components/ui/avatar/avatar';
 import { Icon } from '../../components/ui/icon/icon';
+import { BoardTable } from '../../components/board-table/board-table';
 
 @Component({
   selector: 'app-board',
-  imports: [Button, Avatar, Icon],
+  imports: [Button, Avatar, Icon, BoardTable],
   templateUrl: './board.html',
   styleUrl: './board.scss',
 })
