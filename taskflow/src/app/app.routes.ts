249a9c604f30cf1c454a54fc82023289341e93f0
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2026-06-28 00:45:53 -0300

    feat: register Board and MyTasks routes in app router

--- taskflow/src/app/app.routes.ts
+++ taskflow/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Board } from './pages/board/board';
+import { MyTasks } from './pages/my-tasks/my-tasks';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Board,
+  },
+  {
+    path: 'my-tasks',
+    component: MyTasks,
+  },
+];
