6641efb82c38719efb62842caae235343f1ee99d
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2026-06-29 22:18:50 -0300

    feat: add danger variant to Button component

--- taskflow/src/app/components/ui/button/button.ts
+++ taskflow/src/app/components/ui/button/button.ts
@@ -1,23 +1,27 @@
 import { Attribute, Component } from '@angular/core';
 
 @Component({
-  selector: 'button[primary], button[ghost]',
+  selector: 'button[primary], button[ghost], button[danger]',
   templateUrl: './button.html',
   styleUrl: './button.scss',
   host: {
     '[class.btn-primary]': 'isPrimary',
     '[class.btn-ghost]': 'isGhost',
+    '[class.btn-danger]': 'isDanger',
   },
 })
 export class Button {
   isPrimary = false;
   isGhost = false;
+  isDanger = false;
 
   constructor(
     @Attribute('primary') primary: boolean | null,
     @Attribute('ghost') ghost: boolean | null,
+    @Attribute('danger') danger: boolean | null,
   ) {
     this.isPrimary = primary !== null;
     this.isGhost = ghost !== null;
+    this.isDanger = danger !== null;
   }
 }
