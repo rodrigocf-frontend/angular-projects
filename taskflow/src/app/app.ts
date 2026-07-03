c2e47b493d84531388a2be007c515f869fcba165
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2026-07-03 02:12:02 -0300

    feat: defer modal loading with @defer block

--- taskflow/src/app/app.ts
+++ taskflow/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar';
 import { TopBar } from './components/top-bar/top-bar';
@@ -7,20 +7,16 @@ import { LoadingOverlay } from './components/loading-overlay/loading-overlay';
 import { Snackbar } from './components/ui/snackbar/snackbar';
 import { FormNewProject } from './components/form-new-project/form-new-project';
 import { FormNewTask } from './components/form-new-task/form-new-task';
+import { TaskService } from './services/task-service/task-service';
+import { ProjectService } from './services/project-service/project-service';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    SidebarComponent,
-    TopBar,
-    BottomBar,
-    LoadingOverlay,
-    Snackbar,
-    FormNewProject,
-    FormNewTask,
-  ],
+  imports: [RouterOutlet, SidebarComponent, TopBar, BottomBar, LoadingOverlay, Snackbar, FormNewProject, FormNewTask],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {}
+export class App {
+  taskService = inject(TaskService);
+  projectService = inject(ProjectService);
+}
