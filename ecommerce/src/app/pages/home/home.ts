d14e8544b9d953cfab18441e358bd8c44b27f962
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2026-07-19 11:12:56 -0300

    feat: add Banner component and wire it into Home page

--- ecommerce/src/app/pages/home/home.ts
+++ ecommerce/src/app/pages/home/home.ts
@@ -3,10 +3,11 @@ import { Hero } from './components/hero/hero';
 import { Marquee } from './components/marquee/marquee';
 import { Categories } from './components/categories/categories';
 import { HighlightProducts } from './components/highlight-products/highlight-products';
+import { Banner } from './components/banner/banner';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Marquee, Categories, HighlightProducts],
+  imports: [Hero, Marquee, Categories, HighlightProducts, Banner],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
