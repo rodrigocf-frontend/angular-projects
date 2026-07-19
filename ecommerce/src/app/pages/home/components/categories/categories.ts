5b900808872e918ccff5691efc2b75c87a1fe90f
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2026-07-19 00:42:31 -0300

    feat: add Categories component and wire it into Home page

---
+++ ecommerce/src/app/pages/home/components/categories/categories.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SectionHeader } from '../../../../shared/components/section-header/section-header';
+
+@Component({
+  selector: 'app-categories',
+  imports: [SectionHeader],
+  templateUrl: './categories.html',
+  styleUrl: './categories.scss',
+})
+export class Categories {}
--- ecommerce/src/app/pages/home/home.ts
+++ ecommerce/src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from './components/hero/hero';
 import { Marquee } from './components/marquee/marquee';
+import { Categories } from './components/categories/categories';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Marquee],
+  imports: [Hero, Marquee, Categories],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
