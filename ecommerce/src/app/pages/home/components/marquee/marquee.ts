8ac981f71354a7b85014a9ba573b97bfdafa8728
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2026-07-19 00:03:38 -0300

    feat: add Marquee component and wire it into Home page

---
+++ ecommerce/src/app/pages/home/components/marquee/marquee.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marquee',
+  imports: [],
+  templateUrl: './marquee.html',
+  styleUrl: './marquee.scss',
+})
+export class Marquee {}
--- ecommerce/src/app/pages/home/home.ts
+++ ecommerce/src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Hero } from './components/hero/hero';
+import { Marquee } from './components/marquee/marquee';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero],
+  imports: [Hero, Marquee],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
