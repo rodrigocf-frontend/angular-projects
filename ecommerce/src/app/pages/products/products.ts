47ab92309b569211b1045ba88bc52e421adf02b5
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2026-07-19 21:21:05 -0300

    feat: add ProductsList and Pagination components to Products page main area

--- ecommerce/src/app/pages/products/products.ts
+++ ecommerce/src/app/pages/products/products.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Breadcrumb } from './components/breadcrumb/breadcrumb';
 import { Sidebar } from './components/sidebar/sidebar';
+import { ProductsList } from './components/products-list/products-list';
+import { Pagination } from './components/pagination/pagination';
 
 @Component({
   selector: 'app-products',
-  imports: [Breadcrumb, Sidebar],
+  imports: [Breadcrumb, Sidebar, ProductsList, Pagination],
   templateUrl: './products.html',
   styleUrl: './products.scss',
 })
