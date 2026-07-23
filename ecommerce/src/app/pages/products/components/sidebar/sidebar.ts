935340796be2e8e6a63cfcfc8425cfaac5dbd0aa
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2026-07-23 01:20:30 -0300

    refactor: wire Sidebar and Products page to ProductFilterService

--- ecommerce/src/app/pages/products/components/sidebar/sidebar.ts
+++ ecommerce/src/app/pages/products/components/sidebar/sidebar.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { ProductFilter } from '../../store/products/products.reducer';
+import { ProductFilterService } from '../../../../core/services/product/product-filter.service';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
-export class Sidebar {}
+export class Sidebar {
+  private productsFilterService = inject(ProductFilterService);
+  categories$ = this.productsFilterService.categories$;
+
+  handleFilter(item: ProductFilter) {
+    this.productsFilterService.startFilter(item);
+  }
+  clearFilter() {
+    this.productsFilterService.clearFilter();
+  }
+}
--- ecommerce/src/app/pages/products/products.ts
+++ ecommerce/src/app/pages/products/products.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Breadcrumb } from './components/breadcrumb/breadcrumb';
 import { Sidebar } from './components/sidebar/sidebar';
 import { ProductsList } from './components/products-list/products-list';
 import { Pagination } from './components/pagination/pagination';
+import { ProductFilter } from './store/products/products.reducer';
+import { AsyncPipe } from '@angular/common';
+import { ProductFilterService } from '../../core/services/product/product-filter.service';
 
 @Component({
   selector: 'app-products',
-  imports: [Breadcrumb, Sidebar, ProductsList, Pagination],
+  imports: [Breadcrumb, Sidebar, ProductsList, Pagination, AsyncPipe],
   templateUrl: './products.html',
   styleUrl: './products.scss',
 })
-export default class Products {}
+export default class Products implements OnInit {
+  private productsFilterService = inject(ProductFilterService);
+  filtersActives$ = this.productsFilterService.filtersActives$;
+
+  ngOnInit(): void {
+    this.productsFilterService.syncProducts();
+  }
+
+  handleFilter(item: ProductFilter) {
+    this.productsFilterService.startFilter(item);
+  }
+  clearFilter() {
+    this.productsFilterService.clearFilter();
+  }
+}
