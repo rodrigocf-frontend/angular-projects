d80621a8d4c76f3b6efd0751060b29d81bf3697b
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2026-07-26 19:40:16 -0300

    feat: wire price range inputs to Products filters in Sidebar

--- ecommerce/src/app/pages/products/components/sidebar/sidebar.ts
+++ ecommerce/src/app/pages/products/components/sidebar/sidebar.ts
@@ -1,7 +1,12 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
-import { ProductFilter } from '../../store/products/products.reducer';
+import { ProductFilter } from '../../store/products/products.reducers';
 import { ProductFilterService } from '../../../../core/services/product/product-filter.service';
+import { Store } from '@ngrx/store';
+import { isNumber, tap, toNumber } from 'lodash-es';
+import { FilterType, setFilter } from '../../store/products/products.actions';
+import { selectFilters } from '../../store/products/products.selectors';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,12 +16,58 @@ import { ProductFilterService } from '../../../../core/services/product/product-
 })
 export class Sidebar {
   private productsFilterService = inject(ProductFilterService);
+  private store = inject(Store);
   categories$ = this.productsFilterService.categories$;
+  fromPrice = signal(0);
+  toPrice = signal(0);
+
+  constructor() {
+    this.store
+      .select(selectFilters)
+      .pipe(takeUntilDestroyed())
+      .subscribe({
+        next: (f) => {
+          this.fromPrice.set(f.fromPrice.value);
+          this.toPrice.set(f.toPrice.value);
+        },
+      });
+  }
 
   handleFilter(item: ProductFilter) {
     this.productsFilterService.startFilter(item);
   }
   clearFilter() {
     this.productsFilterService.clearFilter();
+  }
+
+  onChangePrice(value: string, type: 'from' | 'to') {
+    const valueToNumber = toNumber(value);
+    if (isNumber(valueToNumber)) {
+      if (type === 'from') {
+        this.store.dispatch(
+          setFilter({
+            filterType: FilterType.price,
+            price: {
+              type: 'fromPrice',
+              name: '',
+              value: valueToNumber,
+              checked: true,
+            },
+          }),
+        );
+      } else if (type === 'to') {
+        this.store.dispatch(
+          setFilter({
+            filterType: FilterType.price,
+            price: {
+              type: 'toPrice',
+              name: '',
+              value: valueToNumber,
+              checked: true,
+            },
+          }),
+        );
+      }
+    }
   }
 }
