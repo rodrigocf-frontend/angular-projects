e83850211af7b9acb9aaff7db82ba5338a9d5601
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2026-07-27 21:10:44 -0300

    feat: wire sort order selector and format price filter labels in Products page

--- ecommerce/src/app/pages/products/products.ts
+++ ecommerce/src/app/pages/products/products.ts
@@ -1,11 +1,14 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Breadcrumb } from './components/breadcrumb/breadcrumb';
 import { Sidebar } from './components/sidebar/sidebar';
-import { ProductsList } from './components/products-list/products-list';
+import { ListOrder, ProductsList } from './components/products-list/products-list';
 import { Pagination } from './components/pagination/pagination';
-import { ProductFilter } from './store/products/products.reducer';
+import { isPriceFilter, ProductFilter } from './store/products/products.reducers';
 import { AsyncPipe } from '@angular/common';
 import { ProductFilterService } from '../../core/services/product/product-filter.service';
+import { Product } from '../../shared/models/product.model';
+import { isNumber, toNumber } from 'lodash-es';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-products',
@@ -15,7 +18,48 @@ import { ProductFilterService } from '../../core/services/product/product-filter
 })
 export default class Products implements OnInit {
   private productsFilterService = inject(ProductFilterService);
-  filtersActives$ = this.productsFilterService.filtersActives$;
+  filtersActives$ = this.productsFilterService.filtersActives$.pipe(
+    map((value) =>
+      value.map((i) => {
+        if (isPriceFilter(i)) {
+          return {
+            ...i,
+            name: `Min. R$${i.value}`,
+          };
+        }
+        if (isPriceFilter(i)) {
+          return {
+            ...i,
+            name: `Máx. R$ ${i.value}`,
+          };
+        }
+        return i;
+      }),
+    ),
+  );
+  readonly orderedBy = signal<ListOrder>(ListOrder.relevance);
+
+  readonly orderSelects: {
+    label: string;
+    value: ListOrder;
+  }[] = [
+    {
+      label: 'Relevância',
+      value: ListOrder.relevance,
+    },
+    {
+      label: 'Menor Preço',
+      value: ListOrder.minor_price,
+    },
+    {
+      label: 'Maior Preço',
+      value: ListOrder.major_price,
+    },
+    {
+      label: 'Novidades',
+      value: ListOrder.newest,
+    },
+  ];
 
   ngOnInit(): void {
     this.productsFilterService.syncProducts();
@@ -26,5 +70,13 @@ export default class Products implements OnInit {
   }
   clearFilter() {
     this.productsFilterService.clearFilter();
+  }
+
+  onSelectOrder(value: string) {
+    const valueToNumber = toNumber(value);
+    if (valueToNumber) {
+      const findedSelect = this.orderSelects.filter((_, index) => index === valueToNumber)[0];
+      this.orderedBy.set(findedSelect.value);
+    }
   }
 }
