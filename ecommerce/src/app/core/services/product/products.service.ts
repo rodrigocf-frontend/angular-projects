9e7059c57d844f17ff64fd0d49e7b28f3d2c782f
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2026-08-01 20:31:26 -0300

    feat: add category query filtering to ProductService

--- ecommerce/src/app/core/services/product/products.service.ts
+++ ecommerce/src/app/core/services/product/products.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product } from '../../../shared/models/product.model';
+import { CategoryFilter } from '../../../pages/products/store/products/products.reducers';
 
 interface Pagination<T> {
   first: number;
@@ -16,9 +17,28 @@ interface Pagination<T> {
 export class ProductService {
   private http = inject(HttpClient);
 
-  getProducts({ page = 0 }: { page?: number }) {
+  getProducts({ page = 0, categories }: { page?: number; categories?: CategoryFilter[] }) {
+    let params = new HttpParams();
+
+    if (categories) {
+      categories.forEach((category) => {
+        params = params.append('category', this.slugify(category.name));
+      });
+    }
+
+    console.log(params, 'categorieas');
     return this.http.get<Pagination<Product>>(
       `http://localhost:3000/products?_page=${page}&_per_page=9`,
+      {
+        params,
+      },
     );
+  }
+
+  slugify(str: string): string {
+    return str
+      .normalize('NFD') // Separa os acentos das letras (ex: 'á' vira 'a' + ´)
+      .replace(/[\u0300-\u036f]/g, '') // Remove os acentos que foram separados
+      .toLowerCase(); // Passa tudo para minúsculo
   }
 }
